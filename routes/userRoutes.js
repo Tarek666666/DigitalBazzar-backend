20f822a1de5706b3720b563fd36d46a14566e963
Tarek <2b3zab666@gmail.com>
2023-03-30 00:28:22 +0200

    logout

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -33,8 +33,7 @@ userRouter.post("/signin", async (req, res, next) => {
         } else {
             res.clearCookie("token");
             res.clearCookie("loggedIn");
-            res.cookie("loggedIn", false, { expires: 'Thu, 01 Jan 1970 00:00:00 UTC', path: '/' , httpOnly: true, secure: true , sameSite: 'none'});
-            res.cookie("token", jwtToken, { expires: 'Thu, 01 Jan 1970 00:00:00 UTC', path: '/' , httpOnly: true, secure: true , sameSite: 'none'});
+           
             res.json({ msg: "Wrong email or password", success: false });
         }
     } else {
@@ -45,9 +44,10 @@ userRouter.post("/signin", async (req, res, next) => {
 });
 
 userRouter.post("/signout", async (req, res, next) => {
-   
-    res.clearCookie("token");
-   res.clearCookie("loggedIn");
+    res.cookie("loggedIn", false, { expires: 'Thu, 01 Jan 1970 00:00:00 UTC', path: '/' , httpOnly: true, secure: true , sameSite: 'none'});
+    res.cookie("token", jwtToken, { expires: 'Thu, 01 Jan 1970 00:00:00 UTC', path: '/' , httpOnly: true, secure: true , sameSite: 'none'});
+  //  res.clearCookie("token");
+  // res.clearCookie("loggedIn");
     res.status(200).send();
 });
 
