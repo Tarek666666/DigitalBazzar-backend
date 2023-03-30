e0cd4f7454164e77dfb535e8ae7f6e7cf3bd84f1
Tarek <2b3zab666@gmail.com>
2023-03-30 07:49:35 +0200

    installed stripe

--- middleware/isLoggedin.js
+++ middleware/isLoggedin.js
@@ -5,6 +5,7 @@ import userModels from '../models/UsersModel.js';
 const {User} = userModels;
 const isLoggedin = async (req, res, next) => {
     //with each request check if user is logged in , get the user info from db => send it with req
+    console.log('from isloggedin middleware , the req.cookies =========>' , req.cookies)
     if(req.cookies.loggedIn){
         const  token  = req.cookies.token;
         const user = await jwt.verify(token, 'secret');
