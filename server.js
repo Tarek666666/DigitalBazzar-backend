3025784ab8b03894d1d1802c3b6e0017037cbd7e
Tarek <2b3zab666@gmail.com>
2023-03-30 18:27:00 +0200

    auth response

--- server.js
+++ server.js
@@ -26,8 +26,7 @@ app.use(cors({
 
 app.use(express.json());
 
-//with each request check if user is logged in , get the user info from db => send it with req
-//app.use(isLoggedin);
+
 app.get('/auth', async (req,res,next)=>{
 //when app loads , get the user info , to check if user's role if Admin or not
    if (req.user){
@@ -38,6 +37,10 @@ app.get('/auth', async (req,res,next)=>{
     next();
    }
 })
+
+//with each request check if user is logged in , get the user info from db => send it with req
+app.use(isLoggedin);
+
 app.use(stripeRouter)
 
 app.use('/products' , productsRouter);
