ee08e2fbecd398c455195809ef6d0bae20b7d12a
Tarek <2b3zab666@gmail.com>
2023-03-30 21:33:17 +0200

    a

--- middleware/isAdmin.js
+++ middleware/isAdmin.js
@@ -6,7 +6,7 @@ const isAdmin = async (req, res, next) => {
         if (req.user && req.user.role === 'admin') {
             next()
         } else {
-            //case unknown user
+            /// case unknown user
             res.send({isAuth:false})
            
         }
