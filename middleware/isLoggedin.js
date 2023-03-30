e031fb7b6e244cf32a35839bcc04d9833d17b185
Tarek <2b3zab666@gmail.com>
2023-03-30 18:41:32 +0200

    auth response

--- middleware/isLoggedin.js
+++ middleware/isLoggedin.js
@@ -5,12 +5,15 @@ import userModels from '../models/UsersModel.js';
 const {User} = userModels;
 const isLoggedin = async (req, res, next) => {
     //with each request check if user is logged in , get the user info from db => send it with req
-    console.log('from isloggedin middleware , the req.cookies =========>' , req.cookies)
+    
     if(req.cookies.loggedIn){
         const  token  = req.cookies.token;
         const user = await jwt.verify(token, 'secret');
+        console.log('useeeeeeeer from loggedin middleware =========>' , user)
         if(user){
             const loggedUser = await User.findById(user.id);
+
+            console.log('useeeeeeeer from the logged from database middleware =========>' , loggedUser)
             req.user = loggedUser
             next()
         }else{
